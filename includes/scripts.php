9533fed6b1f36c9becc8eaed6eaad2fc4cf9dea1
Maxime Gagné <maxime.gagne@me.com>
2012-09-18 15:50:13 +0200

    fb init

--- includes/scripts.php
+++ includes/scripts.php
@@ -3,7 +3,39 @@
 <script src="../resources/js/plugins.js"></script>
 <script src="../resources/js/main.js"></script>
 <script>
+    app.dependencies = { 
+        facebookSDK: false, 
+        ready: false, 
+    };
+    
+    app.configuration.appId = '<?php echo FB_APP_ID; ?>';
+
+    window.fbAsyncInit = function() {
+        app.dependencies.facebookSDK = true;
+        $(document).trigger('checkdependencies');
+    };
+
+    // Load Facebook SDK Asynchronously
+    (function(d){
+        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement('script'); js.id = id; js.async = true;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        ref.parentNode.insertBefore(js, ref);
+    }(document));
+
     $(document).ready(function() {
+        app.dependencies.ready = true;
+        $(document).trigger('checkdependencies');                
+    })
+    .on('checkdependencies', function() {
+
+        for(var d in app.dependencies) {
+            if(!app.dependencies[d])
+                return;
+        }
+
+        // And we're ready to go, start all the things !
         app.controllers.base = new app.Base();
     });
 </script>
